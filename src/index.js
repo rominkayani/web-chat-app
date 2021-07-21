d3cd148b3bed13fb9df199d18115ea96305f09fb
RKay2000 <rominkayani123@gmail.com>
2021-07-20 21:27:31 -0700

    Created Live  User List for Individual Chat Rooms

--- src/index.js
+++ src/index.js
@@ -42,6 +42,11 @@ io.on('connection', (socket) => {
         socket.emit('message', generateMessage('Admin', 'Welcome!'))
         socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} is here!`))
 
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
+
         callback()
     })
 
@@ -62,7 +67,10 @@ io.on('connection', (socket) => {
 
         if (user) {
             io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left the chat`))
-
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
         }
 
     })
